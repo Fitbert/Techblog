e14e6797e34b7cabb2d6ca036451dd3861a97d28
Albert T <albert.terrazas@gmail.com>
2024-05-20 18:29:55 -0500

    updated for JAwsDB

--- config/config.js
+++ config/config.js
@@ -1,10 +1,18 @@
-const Sequelize = require('sequelize');
+let sequelize;
 
-// Replace the dotenv package with direct configuration
-const sequelize = new Sequelize('techblog_db', 'root', '123456', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
 
 module.exports = sequelize;
